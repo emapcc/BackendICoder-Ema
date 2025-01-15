385802bacd0247027b82261153f699344b3dd547
ema puccio <emapuccio72@gmail.com>
2025-01-14 22:10:35 -0300

    Agregue file system en ProductManager y rutas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,56 @@
 import express from 'express';
+import ProductManager from './productManager.js'
 
 const app = express();
 const PORT = 8080;
 const server = app.listen(PORT, () => console.log("Server listening on port ", PORT));
 
-app.get('/', (req, res) => {
-    res.send("Hola");
+//Para el req.body
+app.use(express.json());
+
+const productManager = new ProductManager('products.json');
+
+//Todos los productos
+app.get('/products', async (req, res) => {
+    const products = await productManager.getProducts();
+    res.json(products);
+});
+
+//Producto por id
+app.get('/products/:pid', async (req,res) => {
+    const product = await productManager.getProductById(req.params.pid);
+    if (!product) return res.status(404).json({ message: 'Producto no encontrado' });
+    res.json(product);
+});
+
+//Agregar producto
+app.post('/products', async (req, res) => {
+    const { title, description, code, price, status = true, stock, category, thumbnails = [] } = req.body;
+    const newProduct = await productManager.addProduct({
+        title,
+        description,
+        code,
+        price,
+        status,
+        stock,
+        category,
+        thumbnails,
+    });
+    res.status(201).json(newProduct);
+});
+
+//Actualizar producto
+app.put('/products/:pid', async (req, res) => {
+    //Toma las actualizaciones
+    const { id, ...actualizaciones } = req.body; // No permitir actualizar el id
+    const prodAct = await productManager.updateProduct(req.params.pid, actualizaciones);
+    if (!prodAct) return res.status(404).json({ message: `Error: Producto con id ${req.params.pid} no encontrado.` });
+    res.json(prodAct);
+});
+
+//Eliminar producto
+app.delete('/products/:pid', async (req, res) => {
+    const deleted = await productManager.deleteProduct(req.params.pid);
+    if (!deleted) return res.status(404).json({ message: 'Producto no encontrado' });
+    res.status(204).send();
 });
--- src/productManager.js
+++ src/productManager.js
@@ -1,77 +1,85 @@
+import { promises as fs } from 'fs';
+
 class ProductManager{
-    static products = [];
-    static contador = 0;
-    
-    constructor(title, description, price, thumbnail, code, stock){
-        this.id;
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock = stock;
+    constructor(nombreArch) {
+        this.filePath = nombreArch;
     }
 
-    addProduct(product) {
-        let productCodeRep = ProductManager.products.find(p => p.code === product.code)
-        if(!productCodeRep && product.title && product.description && product.price && product.thumbnail && product.code && product.stock){
-            ProductManager.contador++; // Incrementa el contador
-            const newProduct = { ...product, id: ProductManager.contador }; // Crea un nuevo producto con el id
-            ProductManager.products.push(newProduct); // Agrega el producto al array estático
-            console.log('Producto creado exitosamente.');
-        }else if(!(product.title && product.description && product.price && product.thumbnail && product.code && product.stock)) return console.log('Error: No estan todos los campos necesarios.');
-        
-        else console.log('Error: Producto con codigo ya existente.');
-        
+    async getProducts() {
+        try{
+            const products = await fs.readFile(this.filePath, 'utf-8');
+            return JSON.parse(products);
+        } catch(error){
+            if (error.code === 'ENOENT') {
+                return [];
+            } else {
+                console.error('Error al leer usuarios:', error);
+                throw error; 
+            }
+        }
     }
 
-    getProducts(){
-        return ProductManager.products;
-    }
-
-    getProductById(id){
-        let productBuscado = ProductManager.products.find(p => p.id === id);
+    async getProductById(id){
+        const products = await this.getProducts();
+        let productBuscado = products.find(p => p.id === id);
         return productBuscado || 'Producto no encontrado.';
     }
+
+    async addProduct(product){
+        try{         
+            const productsFile = await this.getProducts();
+            let productCodeRep = productsFile.find(p => p.code === product.code)
+            if(!productCodeRep && product.title && product.description && product.price && product.status && product.category && product.thumbnails && product.code && product.stock){
+                const newProduct = {
+                    id: `${Date.now()}`,
+                    ...product,
+                };
+                productsFile.push(newProduct); // Agrega el producto al array estático
+                await fs.writeFile(this.filePath, JSON.stringify(productsFile, null, 2));
+                console.log('Producto creado exitosamente.');
+                //return newProduct;
+            }else if(!(product.title && product.description && product.price && product.status && product.category && product.thumbnails && product.code && product.stock)) return console.log('Error: No estan todos los campos necesarios.');
+            
+            else console.log('Error: Producto con codigo ya existente.');
+        }catch(error){
+            console.error('Error al crear el producto.');
+        }
+    }
+
+    async updateProduct(id, actualizaciones){
+        //Agregar try catch?
+        const products = await this.getProducts();
+        const index = products.findIndex(p => p.id === id);
+        //En caso de que no exista el producto
+        if(index === -1){
+            console.log(`Producto con el id ${id} no encontrado.`);
+            return null;
+        } 
+        //Si si existe
+        products[index] = {
+            ...products[index], //Mismos valores
+            ...actualizaciones, //Pisamos valores con actualizaciones
+            id, // El id no cambia
+        };
+        await fs.writeFile(this.filePath, JSON.stringify(products, null, 2));
+        console.log(`Producto actualizado`);
+        return products[index];
+    }
+
+    async deleteProduct(id){
+        const products = await this.getProducts();
+        const updatedProducts = products.filter(p => p.id !== id);
+        //Retorna false si no elimino nada (no encontro el id)
+        if (products.length === updatedProducts.length){
+            console.log('Producto no encontrado.');
+            return false;
+        } 
+        //Actualiza el listado si hay cambio
+        await fs.writeFile(this.filePath, JSON.stringify(updatedProducts, null, 2));
+        console.log('Producto eliminado.');
+        return true;
+    }
+
 };
-
-const manager = new ProductManager();
-
-console.log(manager.getProducts());
-
-//Creacion de producto
-manager.addProduct({
-    title: "producto prueba",
-    description: "Este es un producto prueba",
-    price: 200,
-    thumbnail: "Sin imagen",
-    code: "abc123",
-    stock:25
-});
-//Producto repetido
-manager.addProduct({
-    title: "producto prueba",
-    description: "Este es un producto prueba",
-    price: 200,
-    thumbnail: "Sin imagen",
-    code: "abc123",
-    stock:25
-});
-//Producto con campo faltante
-manager.addProduct({
-    title: "producto prueba",
-    description: "Este es un producto prueba",
-    price: 200,
-    thumbnail: "Sin imagen",
-    code: "abc123",
-});
-
-console.log(manager.getProducts());
-
-console.log('PRODUCTO BUSCADO');
-//Busca producto existente
-console.log(manager.getProductById(1));
-//Busca producto no incluido
-console.log(manager.getProductById(5));
 
 export default ProductManager;
