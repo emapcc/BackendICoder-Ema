94aca326c3c377a2a02dc17b50e6f2205dba01ec
ema puccio <emapuccio72@gmail.com>
2025-02-10 21:15:31 -0300

    Modificación método get usando paginación, filtros y ordenamiento para todos los productos

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,13 +1,45 @@
 import { Router } from 'express';
 import ProductManager from '../fileManager/productManager.js';
+import product from '../models/product.model.js';
 
 const router = Router();
 const productManager = new ProductManager('products.json');
 
 //Todos los productos
-router.get('/', async (req, res) => {
+/* router.get('/', async (req, res) => {
     const products = await productManager.getProducts();
     res.json(products);
+}); */
+//Todos los productos usando paginación, filtros y ordenamiento
+router.get('/', async (req, res) => {
+    const {limit = 10, page = 1, sort, query} = req.query;
+    let filter = {};
+    if (query) {
+        filter = {category: query};
+    }
+    try {
+        const totalProducts = await product.countDocuments(filter);
+        const products = await product.find(filter)
+            .sort(sort ? { price: sort === 'asc' ? 1 : -1 } : {})
+            .skip((page - 1) * limit)
+            .limit(parseInt(limit));
+
+        const totalPages = Math.ceil(totalProducts / limit);
+        res.json({
+            status: 'success',
+            payload: products,
+            totalPages,
+            prevPage: page > 1 ? page - 1 : null,
+            nextPage: page < totalPages ? parseInt(page) + 1 : null,
+            page: parseInt(page),
+            hasPrevPage: page > 1,
+            hasNextPage: page < totalPages,
+            prevLink: page > 1 ? `/api/products?page=${page - 1}&limit=${limit}&query=${query}` : null,
+            nextLink: page < totalPages ? `/api/products?page=${parseInt(page) + 1}&limit=${limit}&query=${query}` : null
+        });
+    }catch(error) {
+        res.status(500).json({ status: 'error', message: error.message });
+    }
 });
 
 //Producto por id
