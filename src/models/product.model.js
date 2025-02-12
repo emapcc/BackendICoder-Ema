6185ff89861f888fa528069d23dcdf5decb8a3f5
ema puccio <emapuccio72@gmail.com>
2025-02-12 13:32:59 -0300

    Actualización router de products para que use mongoose

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -7,7 +7,7 @@ const productCollection = 'products';
 const productSchema = new Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
-    code: {type: String, required: true},
+    code: {type: String, required: true, index: true},
     price: {type: Number, required: true},
     status: {type: Boolean, default: true },
     stock: {type: Number, required: true},
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import ProductManager from '../fileManager/productManager.js';
-import product from '../models/product.model.js';
+import productModel from '../models/product.model.js';
 
 const router = Router();
 const productManager = new ProductManager('products.json');
@@ -13,8 +13,8 @@ router.get('/', async (req, res) => {
         filter = {category: query};
     }
     try {
-        const totalProducts = await product.countDocuments(filter);
-        const products = await product.find(filter)
+        const totalProducts = await productModel.countDocuments(filter);
+        const products = await productModel.find(filter)
             .sort(sort ? { price: sort === 'asc' ? 1 : -1 } : {})
             .skip((page - 1) * limit)
             .limit(parseInt(limit));
@@ -39,44 +39,66 @@ router.get('/', async (req, res) => {
 
 //Producto por id
 router.get('/:pid', async (req,res) => {
-    const product = await productManager.getProductById(req.params.pid);
-    if (!product) return res.status(404).json({ message: 'Producto no encontrado' });
-    res.json(product);
+    const pid = req.params.pid;
+    try {
+        const product = await productModel.findById({_id: pid})
+        if(!product)
+            res.status(404).json({message: `Producto con id ${pid} no encontrado.`})
+        res.json(product);
+    } catch (error) {
+        res.status(500).json({ status: 'error', message: error.message });
+    }
 });
 
 //Agregar producto
 router.post('/', async (req, res) => {
-    let { title, description, code, price, status = true, stock, category, thumbnails = [] } = req.body;
-    price = Number(price);
-    stock = Number(stock);
-    const newProduct = await productManager.addProduct({
-        title,
-        description,
-        code,
-        price,
-        status,
-        stock,
-        category,
-        thumbnails,
-    });
-    if(!newProduct) return res.status(404).json({ message: 'Error al crear el producto.' });
-    res.status(201).json(newProduct);
+    let { title, description, code, price, stock, category} = req.body;
+    if(!title || !description || !code || !price || !stock || !category)
+        return res.status(400).json({message: 'Campos incompletos.'});
+    try {
+        const codeRep = await productModel.findOne({code: code});
+        if(codeRep)
+            return res.status(400).send('Producto con código ya existente.');
+        const newProduct = await productModel.create({
+            title,
+            description,
+            code,
+            price,
+            stock,
+            category
+        });
+        res.status(201).json({status: 'success', payload: newProduct});
+    } catch (error) {
+        res.status(500).json({message: 'Error al crear el producto.', error: error });
+    }
 });
 
 //Actualizar producto
 router.put('/:pid', async (req, res) => {
-    //Toma las actualizaciones
-    const { id, ...actualizaciones } = req.body; // No permitir actualizar el id
-    const prodAct = await productManager.updateProduct(req.params.pid, actualizaciones);
-    if (!prodAct) return res.status(404).json({ message: `Error: Producto con id ${req.params.pid} no encontrado.` });
-    res.json(prodAct);
+    const pid = req.params.pid;
+    const productUpdate = req.body;
+    if(!productUpdate.title || !productUpdate.description || !productUpdate.code || !productUpdate.price || !productUpdate.stock || !productUpdate.category)
+        return res.status(400).json({message: 'Campos incompletos.'});
+    try {
+        const update = await productModel.updateOne({_id: pid}, productUpdate);
+        res.json({status: 'success', payload: update});
+    } catch (error) {
+        res.status(500).json({message: 'Error al actualizar el producto.', error: error });
+    }
 });
 
 //Eliminar producto
 router.delete('/:pid', async (req, res) => {
-    const deleted = await productManager.deleteProduct(req.params.pid);
-    if (!deleted) return res.status(404).json({ message: 'Producto no encontrado' });
-    res.status(204).send();
+    const pid = req.params.pid;
+    try {
+        const product = await productModel.findOne({_id: pid});
+        if(!product)
+            return res.status(400).json({message: `Producto con id ${pid} no encontrado.`});
+        const deleted = await productModel.deleteOne({_id: pid});
+        res.json({status: 'success', payload: deleted});
+    } catch (error) {
+        res.status(500).json({message: 'Error al borrar el producto.', error: error });
+    }
 });
 
 export default router;
