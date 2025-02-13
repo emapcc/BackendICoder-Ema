e683da9428f870fae0a3292c19fdd0c362258f37
ema puccio <emapuccio72@gmail.com>
2025-02-12 22:26:08 -0300

    Modificación plantilla index para que use paginación y creación vista para detalle del product

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,6 +1,7 @@
 import {Router} from 'express';
 import { promises as fs } from 'fs';
 import cartModel from '../models/cart.model.js';
+import productModel from '../models/product.model.js'
 
 const viewsRouter = Router();
 
@@ -12,11 +13,55 @@ const getProducts = async () => {
 }
 
 viewsRouter.get('/products', async (req, res) => {
-    const products = await getProducts();
-    res.render('index', {
-        products,
-        style: 'main.css'
-    });
+    let {limit = 10, page = 1, sort, query} = req.query;
+    page = Number(page);
+    let filter = {};
+    if(query){
+        filter = {category: query};
+    }
+    try {
+        const totalProducts = await productModel.countDocuments(filter);
+        const products = await productModel.find(filter).lean()
+            .sort(sort ? { price: sort === 'asc' ? 1 : -1 } : {})
+            .skip((page - 1) * limit)
+            .limit(parseInt(limit));
+        const totalPages = Math.ceil(totalProducts / limit);
+        const prevPage = page !== 1;
+        const nextPage = totalPages > page;
+        let numberPrev = undefined;
+        let numberSig = undefined;
+        if(1 < page)
+            numberPrev = page - 1;
+        if(page < totalPages)
+            numberSig = page + 1;
+
+        res.render('index', {
+            products,
+            page,
+            query,
+            limit,
+            nextPage,
+            prevPage,
+            numberPrev,
+            numberSig,
+            style: 'main.css'
+        });
+    } catch (error) {
+        res.status(500).json({ status: 'Error al buscar productos.', message: error.message });
+    }
+})
+
+viewsRouter.get('/product/:pid', async (req, res) =>{
+    const pid = req.params.pid;
+    try {
+        const product = await productModel.findById(pid).lean();
+        res.render('product', {
+            product,
+            style: 'main.css'
+        });
+    } catch (error) {
+        res.status(500).json({ status: 'Error al buscar producto.', message: error.message });
+    }
 })
 
 viewsRouter.get('/realtimeproducts', async (req, res) => {
